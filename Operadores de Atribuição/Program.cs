96b7df1aac99b40153ab97a3bf13adec366d5fdf
Evandro2270 <evandro535@gmail.com>
2024-12-03 16:58:06 -0300

    Adicionar arquivos de projeto.

---
+++ Operadores de Atribuição/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int a1 = 10;
+                Console.WriteLine(a1);
+
+                a1 += 2;
+                Console.WriteLine(a1);
+
+                a1 *= 3;
+                Console.WriteLine(a1);
+
+                string s = " SÃO BERNARDO ";
+                Console.WriteLine(s);
+
+                s += "DO CAMPO";
+                Console.WriteLine(s);
+
+                int a = 10;
+                a++;
+                Console.WriteLine(a);
+                
+                int b = 10;
+                b--;
+                Console.WriteLine(b);
+
+            }
+        }
+    }
+}
